5d834b6309a9efab3ab8c58ef4da63a263874c10
subham123-ui <suvam.rockstar@gmail.com>
2026-02-09 11:51:18 +0530

    fixed the small screen view

--- app/(mainLayout)/page.tsx
+++ app/(mainLayout)/page.tsx
@@ -22,8 +22,10 @@ export default async function Home({ searchParams }: SearchParams) {
     ","
   )};location=${location}`;
   return (
-    <div className="grid grid-cols-3 gap-8">
-      <JobFilter />
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+      <div className="lg:col-span-1">
+        <JobFilter />
+      </div>
 
       <div className="col-span-2 flex flex-col gap-6">
         <Suspense fallback={<JobListingLoading />} key={filterKey}>
